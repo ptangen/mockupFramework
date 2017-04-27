a98209fc3f6a6e4d66e305e16dc2157d27c0bef4
ptangen <ptangen@ptangen.com>
2017-04-27 09:53:40 -0400

    Add the next and previous controls to the toolbar.

--- mockupFramework.js
+++ mockupFramework.js
@@ -1,72 +1,11 @@
 (function (lib, img, cjs, ss, an) {
 
 var p; // shortcut to reference prototypes
-lib.webFontTxtInst = {}; 
-var loadedTypekitCount = 0;
-var loadedGoogleCount = 0;
-var gFontsUpdateCacheList = [];
-var tFontsUpdateCacheList = [];
 lib.ssMetadata = [
-		{name:"mockupFramework_atlas_", frames: [[0,0,533,534],[0,536,1180,22]]}
+		{name:"mockupFramework_atlas_", frames: [[0,0,533,534],[535,0,16,16],[553,0,16,16],[0,536,1180,22]]}
 ];
 
 
-
-lib.updateListCache = function (cacheList) {		
-	for(var i = 0; i < cacheList.length; i++) {		
-		if(cacheList[i].cacheCanvas)		
-			cacheList[i].updateCache();		
-	}		
-};		
-
-lib.addElementsToCache = function (textInst, cacheList) {		
-	var cur = textInst;		
-	while(cur != null && cur != exportRoot) {		
-		if(cacheList.indexOf(cur) != -1)		
-			break;		
-		cur = cur.parent;		
-	}		
-	if(cur != exportRoot) {		
-		var cur2 = textInst;		
-		var index = cacheList.indexOf(cur);		
-		while(cur2 != null && cur2 != cur) {		
-			cacheList.splice(index, 0, cur2);		
-			cur2 = cur2.parent;		
-			index++;		
-		}		
-	}		
-	else {		
-		cur = textInst;		
-		while(cur != null && cur != exportRoot) {		
-			cacheList.push(cur);		
-			cur = cur.parent;		
-		}		
-	}		
-};		
-
-lib.gfontAvailable = function(family, totalGoogleCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], gFontsUpdateCacheList);		
-
-	loadedGoogleCount++;		
-	if(loadedGoogleCount == totalGoogleCount) {		
-		lib.updateListCache(gFontsUpdateCacheList);		
-	}		
-};		
-
-lib.tfontAvailable = function(family, totalTypekitCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], tFontsUpdateCacheList);		
-
-	loadedTypekitCount++;		
-	if(loadedTypekitCount == totalTypekitCount) {		
-		lib.updateListCache(tFontsUpdateCacheList);		
-	}		
-};
 // symbols:
 
 
@@ -78,9 +17,23 @@ lib.tfontAvailable = function(family, totalTypekitCount) {
 
 
 
-(lib.toolbarBackground = function() {
+(lib.shuttleleft_ena = function() {
 	this.spriteSheet = ss["mockupFramework_atlas_"];
 	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.shuttleRight = function() {
+	this.spriteSheet = ss["mockupFramework_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.toolbarBackground = function() {
+	this.spriteSheet = ss["mockupFramework_atlas_"];
+	this.gotoAndStop(3);
 }).prototype = p = new cjs.Sprite();
 // helper functions:
 
@@ -101,6 +54,32 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	}
 
 
+(lib.previousButton = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.instance = new lib.shuttleleft_ena();
+	this.instance.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,16,16);
+
+
+(lib.nextButton = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.instance = new lib.shuttleRight();
+	this.instance.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,16,16);
+
+
 (lib.hotspot = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
@@ -115,7 +94,7 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 p.nominalBounds = new cjs.Rectangle(0,0,80,61);
 
 
-(lib.Postit = function(mode,startPosition,loop) {
+(lib.Note = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
 	// Layer 1
@@ -124,7 +103,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,80,61);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-}).prototype = getMCSymbolPrototype(lib.Postit, new cjs.Rectangle(0,0,533,534), null);
+}).prototype = getMCSymbolPrototype(lib.Note, new cjs.Rectangle(0,0,533,534), null);
 
 
 // stage content:
@@ -133,52 +112,19 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 
 	// timeline functions:
 	this.frame_0 = function() {
-		var dataStore = new createjs.Container();
-		
-		dataStore.allNotes = [];
-		var noteInst;
-		
-		function Note (title, content) {
-		    this.title = title;
-		    this.content = content;
-		    this.getInfo = function() {
-		        return this.title + ' ' + this.content + ' note';
-		    };
-		}
-		
-		// note data
-		noteInst = new Note('title 1', 'content 1');
-		dataStore.allNotes.push(noteInst);
-		noteInst = new Note('title 2', 'content 2');
-		dataStore.allNotes.push(noteInst);
-		noteInst = new Note('title 3', 'content 3');
-		dataStore.allNotes.push(noteInst);
-		noteInst = new Note('title 4', 'content 4');
-		dataStore.allNotes.push(noteInst);
-		
-		// mockup data
-		dataStore.mockupLabel = "sample mockup";
-		
-		
-		// set up the mockup
-		
-		// disable the default animation
-		this.stop();
-		
 		// add eventListener to hotspot
 		if (this.hotspot.hasEventListener("click") == false) {
 			this.hotspot.addEventListener("click", clickHotspotHandler.bind(this));
 		}
 		
-		// update text
-		this.titleBarMockupLabel.text = dataStore.mockupLabel;
+		// add eventListener to nextButton
+		if (this.nextButton.hasEventListener("click") == false) {
+			this.nextButton.addEventListener("click", clickHotspotHandler.bind(this));
+		}
 		
-		var initialFrameRequested = getURLParameter("frame"); // try to get value of frame from url
-		
-		if (initialFrameRequested != "") {
-			updateFrame(this, initialFrameRequested);
-		} else {
-			updateFrame(this, null);
+		// add eventListener to previousButton
+		if (this.previousButton.hasEventListener("click") == false) {
+			this.previousButton.addEventListener("click", clickPreviousButtonHandler.bind(this));
 		}
 		
 		function clickHotspotHandler() {
@@ -191,12 +137,23 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 			updateFrame(this, null);
 		}
 		
+		function clickPreviousButtonHandler() {
+		
+			if (this.currentFrame - 1 < 0) {
+				//this.gotoAndStop(this.totalFrames + 1); // if on first frame then go to last frame 
+				updateFrame(this, this.totalFrames);
+			} else {
+				updateFrame(this, this.currentFrame); // go to previous frame
+			}
+		}
+		
 		function updateFrame(thisPassed, frameRequested) {
 			
 			// go to requestedFrame if requested
 			if (frameRequested != null) {
 				frameRequested--;
 				thisPassed.gotoAndStop(frameRequested);
+				console.log("frameRequested = " + frameRequested);
 			}
 			
 			// toolbar
@@ -211,12 +168,69 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 		function getURLParameter (sVar) {
 		  return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 		}
+		
+		
+		var noteInst;
+		
+		function Note (title, content) {
+		    this.title = title;
+		    this.content = content;
+		    this.getInfo = function() {
+		        return this.title + ' ' + this.content + ' note';
+		    };
+		}
+		// initialize the mockup
+		
+		var dataStore = new createjs.Container();
+		dataStore.allNotes = [];
+		
+		// note data
+		noteInst = new Note('title 1', 'content 1');
+		dataStore.allNotes.push(noteInst);
+		noteInst = new Note('title 2', 'content 2');
+		dataStore.allNotes.push(noteInst);
+		noteInst = new Note('title 3', 'content 3');
+		dataStore.allNotes.push(noteInst);
+		noteInst = new Note('title 4', 'content 4');
+		dataStore.allNotes.push(noteInst);
+		
+		// mockup data
+		dataStore.mockupLabel = "sample mockup";
+		
+		// disable the default animation
+		this.stop();
+		
+		// update text
+		this.titleBarMockupLabel.text = dataStore.mockupLabel;
+		
+		// determine which frame to show initially
+		var initialFrameRequested = getURLParameter("frame"); // try to get value of frame from url
+		
+		if (initialFrameRequested != "") {
+			if (parseInt(initialFrameRequested) <= this.totalFrames && parseInt(initialFrameRequested) >= 0) {
+				updateFrame(this, initialFrameRequested);
+			} else {
+				updateFrame(this, null);
+			}
+		} else {
+			updateFrame(this, null);
+		}
 	}
 
 	// actions tween:
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(4));
 
 	// framework
+	this.previousButton = new lib.previousButton();
+	this.previousButton.parent = this;
+	this.previousButton.setTransform(262,670,1,1,0,0,0,8,8);
+	new cjs.ButtonHelper(this.previousButton, 0, 1, 1);
+
+	this.nextButton = new lib.nextButton();
+	this.nextButton.parent = this;
+	this.nextButton.setTransform(367,670,1,1,0,0,0,8,8);
+	new cjs.ButtonHelper(this.nextButton, 0, 1, 1);
+
 	this.noteTitle = new cjs.Text("noteTitle", "bold 12px 'Arial Black'");
 	this.noteTitle.name = "noteTitle";
 	this.noteTitle.lineHeight = 19;
@@ -224,12 +238,13 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	this.noteTitle.parent = this;
 	this.noteTitle.setTransform(986,318.1);
 
-	this.titleBarFrameLabel = new cjs.Text("frame number", "12px 'Arial'");
+	this.titleBarFrameLabel = new cjs.Text("frame num", "11px 'Arial'");
 	this.titleBarFrameLabel.name = "titleBarFrameLabel";
-	this.titleBarFrameLabel.lineHeight = 15;
-	this.titleBarFrameLabel.lineWidth = 191;
+	this.titleBarFrameLabel.textAlign = "center";
+	this.titleBarFrameLabel.lineHeight = 14;
+	this.titleBarFrameLabel.lineWidth = 60;
 	this.titleBarFrameLabel.parent = this;
-	this.titleBarFrameLabel.setTransform(227.1,663.1);
+	this.titleBarFrameLabel.setTransform(314.1,664.1);
 
 	this.noteContent = new cjs.Text("noteContent", "12px 'Arial'");
 	this.noteContent.name = "noteContent";
@@ -237,12 +252,6 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	this.noteContent.lineWidth = 185;
 	this.noteContent.parent = this;
 	this.noteContent.setTransform(986,345.8);
-
-	this.text = new cjs.Text("", "12px 'Arial-Black'");
-	this.text.lineHeight = 19;
-	this.text.lineWidth = 100;
-	this.text.parent = this;
-	this.text.setTransform(101.1,714.7);
 
 	this.titleBarMockupLabel = new cjs.Text("mockupLabel", "bold 12px 'Arial Black'");
 	this.titleBarMockupLabel.name = "titleBarMockupLabel";
@@ -255,11 +264,11 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	this.instance.parent = this;
 	this.instance.setTransform(0,658);
 
-	this.instance_1 = new lib.Postit();
+	this.instance_1 = new lib.Note();
 	this.instance_1.parent = this;
 	this.instance_1.setTransform(1080,412,0.375,0.375,0,0,0,266.5,267);
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance},{t:this.titleBarMockupLabel},{t:this.text},{t:this.noteContent},{t:this.titleBarFrameLabel},{t:this.noteTitle}]}).wait(4));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance},{t:this.titleBarMockupLabel},{t:this.noteContent},{t:this.titleBarFrameLabel},{t:this.noteTitle},{t:this.nextButton},{t:this.previousButton}]}).wait(4));
 
 	// hotspot
 	this.hotspot = new lib.hotspot();
@@ -270,16 +279,16 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	this.timeline.addTween(cjs.Tween.get(this.hotspot).wait(1).to({x:322.5,y:187.5},0).wait(1).to({x:179,y:127.5},0).wait(1).to({x:311.6,y:132.5},0).wait(1));
 
 	// content
-	this.text_1 = new cjs.Text("1", "24px 'Times'", "#0066CC");
-	this.text_1.lineHeight = 26;
-	this.text_1.lineWidth = 67;
-	this.text_1.parent = this;
-	this.text_1.setTransform(330,185.1);
+	this.text = new cjs.Text("1", "24px 'Times'", "#0066CC");
+	this.text.lineHeight = 26;
+	this.text.lineWidth = 67;
+	this.text.parent = this;
+	this.text.setTransform(330,185.1);
 
-	this.timeline.addTween(cjs.Tween.get(this.text_1).wait(1).to({x:320,y:173.5,text:"2"},0).wait(1).to({x:181,y:129.5,text:"3"},0).wait(1).to({x:312,y:118.5,text:"4"},0).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.text).wait(1).to({x:320,y:173.5,text:"2"},0).wait(1).to({x:181,y:129.5,text:"3"},0).wait(1).to({x:312,y:118.5,text:"4"},0).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(590,510.1,1180,563.6);
+p.nominalBounds = new cjs.Rectangle(590,510.1,1180,513);
 // library properties:
 lib.properties = {
 	width: 1180,
@@ -287,9 +296,8 @@ lib.properties = {
 	fps: 24,
 	color: "#FFFFFF",
 	opacity: 1.00,
-	webfonts: {},
 	manifest: [
-		{src:"images/mockupFramework_atlas_.png", id:"mockupFramework_atlas_"}
+		{src:"images/mockupFramework_atlas_.png?1493301059982", id:"mockupFramework_atlas_"}
 	],
 	preloads: []
 };
